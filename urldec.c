0f500e3c8a4eeaf20e0dc0f6494a5de60dc2d7c9
M1cR0xf7 <git@youssefy.com>
2023-02-20 13:16:52 +0200

    clean the code. i found the bug at the memmove line (not surprised)
    i will not fix it. just don't use this software. this bug causes a
    memory leak in the urldec function. encoding is fine though.

--- urldec.c
+++ urldec.c
@@ -8,35 +8,24 @@ void urldec(char *s)
 
     size_t i;
     size_t size;
+    char tmp[4];
 
     char mod = '%';
 
-    char tmp[8];
-
     size = strnlen(s, BUFFER_SIZE-1);
 
-    /* hex delimter */
-    tmp[0]='0';
-    tmp[1]='x';
-
-    /* printf("%c => %c",mod, s[0]); */
-
-    long tmp_ret;
-
     for(i = 0; i < size; ++i){
-	if (s[i] == mod){
-	    /* printf("%c%c%c ", s[i], s[i+1], s[i+2]); */
-	    tmp[2]=s[i+1];
-	    tmp[3]=s[i+2];
-
-	    tmp_ret = hextodecimal(tmp);
-	    printf("%c", (int)tmp_ret);
-
-	    memmove(s+i, s+i+2, strnlen(s, BUFFER_SIZE-1) - i+1);
-	    /* memmove(s+i, s+i+3, strlen(s) - i+2); */
-
-	} else {
+	if (s[i] != mod) {
 	    printf("%c", s[i]);
+	    continue;
 	}
+
+	tmp[0]=s[i+1];
+	tmp[1]=s[i+2];
+
+	printf("%c", (char)hextodecimal(tmp));
+
+	memmove(s+i, s+i+2, size-i+1); /* BUG: NOT SAFE */
+
     }
 }
