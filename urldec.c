98e4ec8c307b25ce22f662d1816517f251f5912e
M1cR0xf7 <git@youssefy.com>
2023-02-20 20:00:14 +0200

    tmp fix printing overflowed data
    this didn't fix the memmove issue.

--- urldec.c
+++ urldec.c
@@ -13,19 +13,40 @@ void urldec(char *s)
     char mod = '%';
 
     size = strnlen(s, BUFFER_SIZE-1);
+    /* sanity check */
+    if (size >= BUFFER_SIZE) {
+	return;			/* bof */
+
+    }
+
+    int nm = 0;
+    for (i = 0; i < size; ++i) {
+	if(s[i] == mod)
+	    nm++;
+    }
+
+    /* string size - number of (modulus sign + 2 hex characters) + null byte */
+    char buf[sizeof(char) * size - 3 * nm + 1];
 
     for(i = 0; i < size; ++i){
 	if (s[i] != mod) {
-	    printf("%c", s[i]);
+	    buf[i] = s[i];
 	    continue;
 	}
 
 	tmp[0]=s[i+1];
 	tmp[1]=s[i+2];
 
-	printf("%c", (char)hextodecimal(tmp));
+	buf[i] = (char)hextodecimal(tmp);
 
 	memmove(s+i, s+i+2, size-i+1); /* BUG: NOT SAFE */
 
     }
+
+    buf[i + 1] = 0;
+
+    for (i = 0; i < strlen(buf); ++i) {
+	printf("%c", buf[i]);
+    }
+
 }
